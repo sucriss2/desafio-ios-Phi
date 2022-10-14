b0aeeb0a495bd0a6d5fa24deeefc0c155bc32d11
Suelen Critina Vaz <sucris12@gmail.com>
2022-10-13 21:06:07 -0300

    Create test StatementModel.

---
+++ Desafio_Somosphi/Desafio_SomosphiTests/StatementModelTests.swift
@@ -0,0 +1,22 @@
+//
+//  StatementViewControllerTests.swift
+//  Desafio_SomosphiTests
+//
+//  Created by Suh on 07/10/22.
+//
+
+import XCTest
+@testable import Desafio_Somosphi
+
+final class StatementViewControllerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+
+}
